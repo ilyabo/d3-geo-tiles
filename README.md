9e40e2a1c24c9bc227d091d7082fcd59cc7463ec
ilyabo <Ilya Boyandin>
2015-08-10 10:27:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Zoomable Tiles
+
+This is basically a copy of Mike Bostock's [geo tile plugin](https://github.com/d3/d3-plugins/tree/master/geo/tile)
+created to be easier to require as an npm module.
+
+A D3 layout for determining which 256x256 quadtree tiles to display in a rectangular viewport,
+based on a scale and translate. This layout can be used to create a simple slippy map, or
+render standard map tiles (e.g., MapBox, CloudMade) as a base layer behind a geographic projection. 
+
+# Examples
+
+* [Panning & Zooming](http://bl.ocks.org/mbostock/4132797)
+* [Clipping](http://bl.ocks.org/mbostock/4150951)
+* [Vector Tiles](http://bl.ocks.org/mbostock/5593150)
+* [Raster Tiles & Vector Overlay](http://bl.ocks.org/mbostock/5342063)
---
+++ index.js
@@ -0,0 +1,102 @@
+var tile = function() {
+  var size = [960, 500],
+      scale = 256,
+      translate = [size[0] / 2, size[1] / 2],
+      zoomDelta = 0;
+
+  function tile() {
+    var z = Math.max(Math.log(scale) / Math.LN2 - 8, 0),
+        z0 = Math.round(z + zoomDelta),
+        k = Math.pow(2, z - z0 + 8),
+        origin = [(translate[0] - scale / 2) / k, (translate[1] - scale / 2) / k],
+        tiles = [],
+        cols = d3.range(Math.max(0, Math.floor(-origin[0])), Math.max(0, Math.ceil(size[0] / k - origin[0]))),
+        rows = d3.range(Math.max(0, Math.floor(-origin[1])), Math.max(0, Math.ceil(size[1] / k - origin[1])));
+
+    rows.forEach(function(y) {
+      cols.forEach(function(x) {
+        tiles.push([x, y, z0]);
+      });
+    });
+
+    tiles.translate = origin;
+    tiles.scale = k;
+
+    return tiles;
+  }
+
+  tile.size = function(_) {
+    if (!arguments.length) return size;
+    size = _;
+    return tile;
+  };
+
+  tile.scale = function(_) {
+    if (!arguments.length) return scale;
+    scale = _;
+    return tile;
+  };
+
+  tile.translate = function(_) {
+    if (!arguments.length) return translate;
+    translate = _;
+    return tile;
+  };
+
+  tile.zoomDelta = function(_) {
+    if (!arguments.length) return zoomDelta;
+    zoomDelta = +_;
+    return tile;
+  };
+
+  return tile;
+};
+
+
+
+
+// Utility functions
+// http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#ECMAScript_.28JavaScript.2FActionScript.2C_etc..29
+
+// coords -> tile
+function long2tile(lon, zoom) {
+  return (Math.floor((lon+180)/360*Math.pow(2,zoom)));
+}
+
+function lat2tile(lat, zoom)  {
+  return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom)));
+}
+
+
+// tile -> bounding box
+function tile2long(x, z) {
+ return (x/Math.pow(2,z)*360-180);
+}
+
+function tile2lat(y, z) {
+ var n = Math.PI-2*Math.PI*y/Math.pow(2,z);
+ return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+}
+
+
+
+
+tile.locationToTile = function(lat, lon, zoom) {
+  return {
+    x: long2tile(lon, zoom),
+    y: lat2tile(lon, zoom),
+    z: zoom
+  };
+};
+
+tile.tileToBoundingBox = function(x, y, z) {
+  return [
+    [tile2lon(x, z), tile2lat(y, z)],
+    [tile2lon(x, z), tile2lat(y, z)]
+  ];
+};
+
+
+
+
+module.exports = tile;
