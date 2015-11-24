fb0c936f6501db1dffef7333685d0f042b841713
ilyabo <Ilya Boyandin>
2015-11-24 13:58:40 +0100

    tileToBoundingBox was failing because of too precise fp check

--- test/bounding-box-test.js
+++ test/bounding-box-test.js
@@ -4,9 +4,11 @@ var tape = require("tape"),
 tape("tileToBoundingBox can calculate bounding box for given tile coords", function(test) {
 
   var bb = geoTiles.tileToBoundingBox(1207, 1539, 12);
+  bb = bb.map(function(dd) { return dd.map(function(d) { return d.toFixed(5) }) })
+
   test.deepEqual(
     bb,
-    [ [ -73.916015625, 40.78054143186031 ], [ -73.828125, 40.71395582628604 ] ]
+    [ [ '-73.91602', '40.78054' ], [ '-73.82813', '40.71396' ] ]
   );
 
   test.end();
